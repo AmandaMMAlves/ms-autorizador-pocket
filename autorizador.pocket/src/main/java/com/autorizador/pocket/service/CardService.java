19c2c8f3b1f8121d0aafc01eaf005ec45a74b653
Amanda Mayara <amanda.mayaraa@gmail.com>
2022-07-11 22:27:13 -0300

    Remove itens não usados e adiciona synchronized em metodo de transacao

--- autorizador.pocket/src/main/java/com/autorizador/pocket/service/CardService.java
+++ autorizador.pocket/src/main/java/com/autorizador/pocket/service/CardService.java
@@ -5,8 +5,6 @@ import com.autorizador.pocket.exception.CardNotFoundException;
 import com.autorizador.pocket.mapper.CardMapper;
 import com.autorizador.pocket.model.Card;
 import com.autorizador.pocket.repository.CardRepository;
-import com.autorizador.pocket.http.request.CardRequest;
-import com.autorizador.pocket.http.response.CardResponse;
 import com.autorizador.pocket.service.validator.BalanceValidator;
 import com.autorizador.pocket.service.validator.CardExistValidator;
 import com.autorizador.pocket.service.validator.CardTransactionValidator;
@@ -15,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 /**
  * Service responsável pelo tráfego dos cartões
@@ -25,7 +22,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CardService {
     private final CardRepository repository;
-    private final CardMapper mapper;
 
     private final CardTransactionValidator transactionValidator = new CardExistValidator();
 
@@ -42,31 +38,10 @@ public class CardService {
             .orElseThrow(CardNotFoundException::new);
     }
 
-    public boolean authorizeCardTransaction(String cardNumber, String password, BigDecimal value){
+    public synchronized boolean authorizeCardTransaction(String cardNumber, String password, BigDecimal value){
         transactionValidator.setNext(new PasswordValidator()).setNext(new BalanceValidator(repository));
         return transactionValidator.validate(repository.findByCardNumber(cardNumber), password, value);
     }
-
-    public List<CardResponse> getAllCards() {
-        var cards = repository.findAll();
-        return cards.stream().map(mapper::modelToResponse).toList();
-    }
-
-    public void deleteCard(CardRequest request) {
-        Card card = mapper.requestToModel(request);
-        repository.delete(card);
-    }
-
-    public void deleteAllCards(){
-        repository.deleteAll();
-    }
-
-    private CardResponse initCard(CardRequest request) {
-        Card newCard = mapper.requestToModel(request);
-        newCard.setBalance(BigDecimal.valueOf(500L));
-        return mapper.modelToResponse(repository.save(newCard));
-    }
-
     private Card initCard(Card newCard) {
         newCard.setBalance(BigDecimal.valueOf(500L));
         return repository.save(newCard);
